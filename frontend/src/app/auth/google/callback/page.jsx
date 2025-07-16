4c54e498f9bff8ee6407454a0eba31d9741c77f5
Rubina Pokhrel <pokhrelrubina2061@gmail.com>
2025-07-16 06:37:17 +0545

    update code

--- frontend/src/app/auth/google/callback/page.jsx
+++ frontend/src/app/auth/google/callback/page.jsx
@@ -1,42 +1,11 @@
-// "use client";
-
-// import { useSearchParams, useRouter } from "next/navigation";
-// import { useEffect } from "react";
-
-// const GoogleAuthCallback = () => {
-//   const searchParams = useSearchParams();
-//   const router = useRouter();
-
-//   useEffect(() => {
-//     const token = searchParams.get("token");
-//     const role = searchParams.get("role");
-
-//     if (token && role) {
-//       localStorage.setItem("authToken", token);
-//       localStorage.setItem("role", role);
-
-//       if (role === "admin") {
-//         router.push("/admin/dashboard");
-//       } else {
-//         router.push("/");
-//       }
-//     } else {
-//       alert("Authentication failed. Please try again.");
-//     }
-//   }, [searchParams, router]);
-
-//   return <div>Authenticating...</div>;
-// };
-
-// export default GoogleAuthCallback;
-
-export const dynamic = "force-dynamic";
-("use client");
+"use client";
 
 import Loading from "@/components/Loading";
 import { useSearchParams, useRouter } from "next/navigation";
 import { useEffect, Suspense } from "react";
 import toast from "react-hot-toast";
+
+export const dynamic = "force-dynamic";
 
 function CallbackInner() {
   const searchParams = useSearchParams();
